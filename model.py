d581765826056baef1c3b42217002bd050a6f9fa
Arthur.mf <arthurmf.amf@gmail.com>
2019-03-07 15:00:35 +0100

    Python file
    Plan for python file

---
+++ model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 14:56:14 2019
+
+@author: arthurmaroquenefroissart
+"""
+#%%
+
+#ANALYSIS OF VaR using Spark/PYTHON
+
+#GET THE DATA 
+
+#USE MODEL OF THE RETURN BASED ON CERTAIN FACTORS (S&P, NASDAQ)
+
+#SAMPLE THE RETURNS OF EACH INSTRUMENTS 
+
+#SAMPLING THE POSSIBLE VALUE OF THE OUTCOMES 
+
+#CALCULATE VaR of the PORTEFEUILLE
+
+
+
