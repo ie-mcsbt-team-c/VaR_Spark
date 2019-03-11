54be4a2da584f3dfb2cc152be6605cb2c2c30a3c
Hannah O <h.oldorf@hotmail.de>
2019-03-11 16:22:08 +0100

    Detecting Null Values (Factors)
    I separated the df for the four factors and checked for null values, as well compared the 'timestamp' column to make sure there are no discrpancies in the pulled dates.

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ Created on Thu Mar  7 14:56:14 2019
 
 #ANALYSIS OF VaR using Spark/PYTHON
 
-import quandl
 import urllib.request as url
 import pandas as pd 
 
@@ -28,16 +27,16 @@ api_key_alpha  = 'SQ60DKQWSFAU53XH'
 api_key_quandl = 'ujUGW2cbgsDmD7sP359j'
 
 
-urlsa = [ 
-        'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=^GSPC&outputsize=full&apikey=', 
-        'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NDAQ&outputsize=full&apikey=',
-        ]
+urlsaGSP = ['https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=^GSPC&outputsize=full&apikey=']
 
-urlsq = ['https://www.quandl.com/api/v3/datasets/OPEC/ORB/data.csv?start_date=' + start_date + '&end_date=' + end_date + '&api_key=' + api_key_quandl, 
-         'https://www.quandl.com/api/v3/datasets/USTREASURY/YIELD.csv?start_date=' + start_date + '&end_date=' + end_date + '&api_key=' + api_key_quandl]
+urlsaNDAQ = ['https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NDAQ&outputsize=full&apikey=']
+
+urlsqOPEC = ['https://www.quandl.com/api/v3/datasets/OPEC/ORB/data.csv?start_date=' + start_date + '&end_date=' + end_date + '&api_key=' + api_key_quandl]
+
+urlsqUSTREASURY = ['https://www.quandl.com/api/v3/datasets/USTREASURY/YIELD.csv?start_date=' + start_date + '&end_date=' + end_date + '&api_key=' + api_key_quandl]
 
 
-for link in urlsa: 
+for link in urlsaGSP: 
     
     remoteFile = url.urlopen(link + api_key_alpha + '&datatype=csv')
     print(remoteFile)
@@ -45,21 +44,70 @@ for link in urlsa:
     b = pd.DataFrame(data=html)
     b = b[0].str.split(",", expand = True)
     b.columns = b.iloc[0]
-    b = b[1:]
+    b = b[1:] # b = GSP Dataframe
+
+for link2 in urlsaNDAQ: 
+    
+    remoteFile = url.urlopen(link + api_key_alpha + '&datatype=csv')
+    print(remoteFile)
+    html = remoteFile.read().decode('ascii').splitlines()
+    c = pd.DataFrame(data=html)
+    c = c[0].str.split(",", expand = True)
+    c.columns = c.iloc[0]
+    c = c[1:] # c = NASDAQ Dataframe
     
     
-for links2 in urlsq: 
+    
+for link3 in urlsqOPEC: 
     remoteFile = url.urlopen(link + api_key_quandl + '&datatype=csv' )
     print(remoteFile)
     html1 = remoteFile.read().decode('ascii').splitlines()
-    c = pd.DataFrame(data=html1)
-    c = c[0].str.split(",", expand = True)
-    c.columns = c.iloc[0]
-    c = c[1:]
+    d = pd.DataFrame(data=html1)
+    d = d[0].str.split(",", expand = True)
+    d.columns = d.iloc[0]
+    d = d[1:] # d = OPEC Dataframe
+
+
+for link4 in urlsqUSTREASURY: 
+    remoteFile = url.urlopen(link + api_key_quandl + '&datatype=csv' )
+    print(remoteFile)
+    html1 = remoteFile.read().decode('ascii').splitlines()
+    e = pd.DataFrame(data=html1)
+    e = e[0].str.split(",", expand = True)
+    e.columns = e.iloc[0]
+    e = e[1:] # e = US Treasury Dataframe
     
 
 
-#%%
+#%% DETECTING NULL VALUES  
+
+# df.info() command tells us the total number of non null observations including the total number of entries. 
+# Once number of entries isn’t equal to number of non null observations, we can suspect missing values.
+    
+
+# No null values in factor dataframes 
+# Total number of entries = non-null observations = 4826 entries
+
+b.info()
+c.info()
+d.info()
+e.info()
+
+
+b.isnull().sum()
+c.isnull().sum()
+d.isnull().sum()
+e.isnull().sum()
+
+
+# we are comparing the 'timestamp' column of the dataframes with each other, 
+# to make sure they match across all df we pulled. False indicates matching values (ne = not equal) 
+
+datecheck = c.timestamp.ne(d.timestamp, e.timestamp)
+datecheck.unique() #output = array([False])
+
+    
+#%%   
 #GET THE DATA 
 
 #USE MODEL OF THE RETURN BASED ON CERTAIN FACTORS (S&P, NASDAQ)
