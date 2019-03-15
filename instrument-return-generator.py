81c4f886a0025a0d039d0a0601d80a5836be3dfa
leilatazi <44499508+leilatazi@users.noreply.github.com>
2019-03-15 16:50:27 +0100

    instruments_returns.csv

--- instrument-return-generator.py
+++ instrument-return-generator.py
@@ -19,63 +19,111 @@ instruments = symbols.split()
 #%%
 
 
-print(instruments)
+instruments = ["AAON","ABAX","ACTA","AEGR","BCOM","BOSC","CFFI","CHOP","CHRW","CRMT","DGLD","DJCO","DOX","EGHT","FCCY","FCTY","FLWS","FOLD","FTLB","FUBC","GOOG","HGSH","HTHT","IMOS","INBK","JOBS","JRJC","KOOL","MDRX","MTGE","MULT","PIH","PLCE","QABA","SATS","SRCE","TWOU","UHAL","VNET","WATT","XLRN"]
 
 d = {}
 
-for instrument in instruments: 
-    print(instrument)
-    df = pd.read_csv("https://raw.githubusercontent.com/ie-mcsbt-team-c/VaR_Spark/master/instruments/{}.csv". format(instrument))
+for ins in instruments: 
+
+    df = pd.read_csv("https://raw.githubusercontent.com/ie-mcsbt-team-c/VaR_Spark/master/instruments/{}.csv". format(ins))
     
-    d[instrument] = pd.DataFrame(df)
+    d[ins] = pd.DataFrame(df)
     
+dataframes = d
 
-
-dataframes = d_instruments
-
-for dataf in dataframes: 
+for df in dataframes: 
     
     i = 0
-    prodf = []
-    for row in dataframes[dataf].itertuples(): 
+    pf = []
+    
+    for row in dataframes[df].itertuples(): 
         row = pd.DataFrame([row])
-        if i < (len(dataframes[dataf]) - 5):
+        if i < (len(dataframes[df]) - 5):
 #            if dataf == (b) | (c): 
         
-            x = dataframes[dataf].iloc[i]
+            x = dataframes[df].iloc[i]
             x1 = x['close']
             x2 = pd.to_numeric(x1)
-            print(x2)
             
-            y = dataframes[dataf].iloc[i + 4]
+            y = dataframes[df].iloc[i + 4]
             y1 = y['open']
             y2 = pd.to_numeric(y1)
-            print(y2)
-                
-                
+            
             ret = ((x2 - y2) / y2).item()
-            print(ret)
-            prodf.append(ret)
+
+            pf.append(ret)
             i = i + 1
         
-    prodf = pd.DataFrame(prodf)
-    print(prodf)  
-    dataframes[dataf]['return'] = prodf
-    
-    print(dataf)
-
+    pf = pd.DataFrame(pf)
+  
+    dataframes[df]['return'] = pf
         
 #%%    
         
-for dataf in dataframes: 
-    for col in dataframes[dataf]: 
-        dataframes[dataf] = dataframes[dataf][['timestamp', 'return']]
-        dataframes[dataf].rename(columns={'timestamp':'Date'}, inplace=True)
+for df in dataframes:
 
+    dataframes[df] = dataframes[df][['timestamp', 'return']]
+    dataframes[df].rename(columns={'timestamp':'Date'}, inplace=True)
 
-
-        
-        
 
  #%%
+#Create instruments_returns
+#dz = pd.DataFrame(columns=["Date"])
+#
+#dz['Date']= pd.to_datetime(dz['Date'])
+
+dz = pd.bdate_range('2008-01-01', '2019-01-31')
+dz = pd.DataFrame(dz,columns=['Date'])
+
+dataframes["AAON"]['Date']= pd.to_datetime(dataframes["AAON"]['Date'])
+dz = pd.merge(dz,dataframes["AAON"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["ABAX"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["ACTA"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["AEGR"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["BCOM"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["BOSC"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["CFFI"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["CHOP"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["CHRW"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["CRMT"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["DGLD"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["DJCO"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["DOX"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["EGHT"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["FCCY"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["FCTY"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["FLWS"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["FOLD"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["FTLB"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["FUBC"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["GOOG"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["HGSH"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["HTHT"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["IMOS"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["INBK"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["JOBS"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["JRJC"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["KOOL"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["MDRX"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["MTGE"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["MULT"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["PIH"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["PLCE"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["QABA"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["SATS"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["SRCE"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["TWOU"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["UHAL"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["VNET"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["WATT"], how = 'outer', on=['Date'])
+dz = pd.merge(dz,dataframes["XLRN"], how = 'outer', on=['Date'])
+
+instruments_returns = dz
+export_csv = instruments_returns.to_csv(r"C:\Users\Leila\Desktop\VaR_Spark\instruments_returns.csv", index = None, header=True) #Don't forget to add '.csv' at the end of the path
+
+#for df in dataframes :    
+#    
+#
+#    dataframes[df]['Date']= pd.to_datetime(dataframes[df]['Date'])
+#    dz = pd.merge(dz,dataframes[df], how = 'inner', on=['Date'])
 
