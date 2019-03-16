3b250247fa1f1ff13aea573ffb454f1ccab46f05
Dv <Dv@iMac-de-Dv.local>
2019-03-16 13:44:31 +0100

    xx

--- featurize-david.py
+++ featurize-david.py
@@ -14,12 +14,12 @@ import seaborn as sbn
 import matplotlib.pyplot as plt
 import pandas as pd 
 import statsmodels.api as sm
-#%% Read Factors Returns
+#%% Read Factors Returns 
 
 
 
 factors_returns = pd.read_csv("https://raw.githubusercontent.com/ie-mcsbt-team-c/VaR_Spark/master/factors_returns.csv")
-instruments_returns = pd.read_csv("https://raw.githubusercontent.com/ie-mcsbt-team-c/VaR_Spark/master/instrument_returns.csv")
+instruments_returns = pd.read_csv("https://raw.githubusercontent.com/ie-mcsbt-team-c/VaR_Spark/master/instruments_returns.csv")
 factors_returns = pd.DataFrame(factors_returns)
 factors_returns=factors_returns.drop(["Date"], axis=1)
 print(factors_returns)
@@ -36,12 +36,14 @@ def featurize(factors_returns):
        
        return result
 
-featurize_returns = pd.DataFrame(featurize(factors_returns))
+factors_featurize_returns = pd.DataFrame(featurize(factors_returns))
 
 
 #%%
 def estimateParams(y, x):
     return sm.OLS(y, x).fit().params
 
+factors_featurize_returns_intercept = sm.add_constant(factors_featurize_returns, prepend=True)
 
+weights = [estimateParams(stockReturns, factor_columns) for stockReturns in stocksReturns] 
 
