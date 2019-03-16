49c60084686e5cc01c06f17cd8180402725bfcdc
Dv <Dv@iMac-de-Dv.local>
2019-03-16 13:21:43 +0100

    featurize returns

---
+++ featurize-david.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 14:56:14 2019
+
+@author: arthurmaroquenefroissart
+"""
+#%%
+
+import urllib.request as url
+import pandas as pd 
+import numpy as np
+import seaborn as sbn
+import matplotlib.pyplot as plt
+import pandas as pd 
+import statsmodels.api as sm
+#%% Read Factors Returns
+
+
+
+factors_returns = pd.read_csv("https://raw.githubusercontent.com/ie-mcsbt-team-c/VaR_Spark/master/factors_returns.csv")
+instruments_returns = pd.read_csv("https://raw.githubusercontent.com/ie-mcsbt-team-c/VaR_Spark/master/instrument_returns.csv")
+factors_returns = pd.DataFrame(factors_returns)
+factors_returns=factors_returns.drop(["Date"], axis=1)
+print(factors_returns)
+#%%
+
+#%%
+
+def featurize(factors_returns):
+       original = factors_returns
+       sign = np.sign(factors_returns)
+       power = np.power(factors_returns,2)
+       root = np.power(abs(factors_returns),0.5)
+       result = sign*(original*power+root)
+       
+       return result
+
+featurize_returns = pd.DataFrame(featurize(factors_returns))
+
+
+#%%
+def estimateParams(y, x):
+    return sm.OLS(y, x).fit().params
+
+
+
