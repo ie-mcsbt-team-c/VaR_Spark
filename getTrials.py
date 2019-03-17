9d09ac61272232f5004a1e5192183d8490f905bd
leilatazi <44499508+leilatazi@users.noreply.github.com>
2019-03-17 15:45:30 +0100

    Update getTrials.py

--- getTrials.py
+++ getTrials.py
@@ -7,6 +7,7 @@ Created on Sun Mar 17 13:12:07 2019
 
 #%%
 
+import numpy as np
 from pyspark.sql import SparkSession
 
 spark = SparkSession \
@@ -14,6 +15,35 @@ spark = SparkSession \
     .appName("basic") \
     .getOrCreate()
 
-df = spark.read.csv("mean.csv",header=True,sep="\n");
+mean = spark.read.csv("mean.csv",header=False,sep="\n");
+cov = spark.read.csv("cov.csv",header=False,sep="\n");
+coefs = spark.read.csv("coefs.csv",header=False,sep="\n");
+inter = spark.read.csv("inter.csv",header=False,sep="\n");
 
-print(df.collect())
+def generate_trial(mean,cov):
+    
+    #creating an empty list to store the returns
+    
+#    trialReturns = []
+    
+    #creating a loop to go over t number of trials
+
+#    for i in range(0, t): 
+         
+    #initializing the total portfolio return
+            
+#            trial_portfolioReturn = 0
+            
+    #generating one sample of 4 factors
+            
+    trial_factorReturns = np.random.multivariate_normal(mean, cov)
+    
+    print(trial_factorReturns)
+    
+generate_trial(mean,cov)
+
+spark.stop()
+
+
+            
+
